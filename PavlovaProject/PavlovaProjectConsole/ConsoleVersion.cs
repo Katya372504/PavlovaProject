3e3969241be405854ff48f38632ed39d6ee8464c
Katya <katp97@mail.ru>
2017-06-13 20:59:51 +0300

    the fifth version

---
+++ PavlovaProject/PavlovaProjectConsole/ConsoleVersion.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using WikipediaNET;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace PavlovaProjectConsole
+{
+    /// <summary>
+    /// Основная программа.
+    /// </summary>
+    class ConsoleVersion
+    {
+        /// <summary>
+        /// Поток исполнения программы.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //Объект из библиотеки Wikipedia.Net. (https://github.com/Genbox/Wikipedia.Net)
+            Wikipedia wikipedia = new Wikipedia();
+               
+            while (true)
+            {
+                //Часть ввода.
+                Console.Write("Как много ссылок попытаться найти:");
+
+                /// <summary>
+                /// Число искомых ссылок.
+                /// </summary>
+                int count = Convert.ToInt32(Console.ReadLine());
+                wikipedia.Limit = count;
+
+                Console.Write("Я хочу знать:");
+
+                /// <summary>
+                ///Запрос.
+                /// </summary>
+                string t = Console.ReadLine();
+                
+
+                //Результаты поиска по переданному запросу.
+                //Объект из библиотеки Wikipedia.Net. (https://github.com/Genbox/Wikipedia.Net)
+                WikipediaNET.Objects.QueryResult results = wikipedia.Search(t);
+
+                Console.WriteLine("Вывод результатов: ...");
+
+                //Цикл вывода результатов.
+                foreach (WikipediaNET.Objects.Search s in results.Search)
+                    Process.Start(s.Url.ToString());
+
+                //Взаимодействие с пользователем.
+                Console.WriteLine("Хотите завершить работу? (Да - нажмите N, иначе любую кнопку.)");
+
+                if (Console.ReadKey(true).Key == ConsoleKey.N)
+                {
+                    Console.Write("Спасибо за использование.");
+                    //Задержка перед выходом.
+                    Thread.Sleep(2000);
+                    return;
+                }
+                else
+                {
+                    //Продолжение работы.
+                    Console.ReadKey(true);
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
--- PavlovaProject/WindowsFormsPavlovaProject/Form1.cs
+++ PavlovaProject/WindowsFormsPavlovaProject/Form1.cs
@@ -12,25 +12,41 @@ using HtmlAgilityPack;
 
 
 namespace WindowsFormsPavlovaProject
-{
+{   
+    /// <summary>
+    /// Форма программы "Незнайка".
+    /// </summary>
     public partial class MainForm : Form
     {
         public MainForm()
         {
             InitializeComponent();
         }
-
+        /// <summary>
+        /// Объект для работы с поиском.
+        /// </summary>
         Wikipedia wikipedia = new Wikipedia();
-       
+       /// <summary>
+       /// Нажатие кнопки "Найти". Поиск результата.
+       /// </summary>
+       /// <param name="sender"></param>
+       /// <param name="e"></param>
         private void  SearchButton_Click(object sender, EventArgs e)
         {
+            //Очистить поле вывода от предыдущего результата.
             TextBox.Clear();
+            //Задаем количество искомых ссылок.
             wikipedia.Limit = 1;
+            //Формируем объект, содержащий результаты поиска.
             WikipediaNET.Objects.QueryResult results = wikipedia.Search(SearchText.Text);
+            //Формируем список ссылок.
             WikipediaNET.Objects.Search link = results.Search[0];
             
             string html = link.Url.OriginalString;
+
+            //Создаем объект для работы со страницей HTML (Библиотека HtmlAgilityPack)
             HtmlAgilityPack.HtmlDocument HD = new HtmlAgilityPack.HtmlDocument();
+
             var web = new HtmlWeb
             {
                 AutoDetectEncoding = false,
@@ -38,26 +54,33 @@ namespace WindowsFormsPavlovaProject
             };
             HD = web.Load(html);                    //Скачиваем всю HTML страницу
 
+            //Выбор по наличию блока, содержащего класс "mw-content-ltr" (класс содержащий текст статьи в разметке HTML)
             HtmlNodeCollection NoAltElements;
-            NoAltElements = HD.DocumentNode.SelectNodes("//div[@class='mw-content-ltr']/p");
-            
-                                                                                             
+            NoAltElements = HD.DocumentNode.SelectNodes("//div[@class='mw-content-ltr']/p"); 
 
-            string outputText = "";
+
+            //Пустая строка для записи текста.
+            string outputText = string.Empty; 
+
             // проверка на наличие найденных узлов
             if (NoAltElements != null)
             {
                 foreach (HtmlNode HN in NoAltElements)
                 {
-                    //Получаем строчки
+                    //Получаем строчки из HTML-файла.
                     outputText = HN.InnerText;
+                    //Добавляем строчки в TextBox.
                     TextBox.AppendText(outputText);
                 }
           
             }
             
         }
-
+        /// <summary>
+        /// Включение поиска по нажатию Enter.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void SearchText_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Enter)
@@ -65,25 +88,37 @@ namespace WindowsFormsPavlovaProject
                 SearchButton_Click(this, new EventArgs());
             }
         }
-
+        /// <summary>
+        /// Кнопка смены языка поиска (русский язык).
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void RussianSearchButton_Click(object sender, EventArgs e)
         {
-            wikipedia.Language = WikipediaNET.Enums.Language.Russian;
+            wikipedia.Language = WikipediaNET.Enums.Language.Russian;  //Смена языка поиска на русский (Wikipedia.Net библиотека).
             SearchText.Text = "Введите запрос (Русскоязычный поиск)";
         }
-
+        /// <summary>
+        /// Кнопка смены языка поиска (английский язык).
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void EnglishSearchButton_Click(object sender, EventArgs e)
         {
-            wikipedia.Language = WikipediaNET.Enums.Language.English;
+            wikipedia.Language = WikipediaNET.Enums.Language.English; //Смена языка поиска на английский (Wikipedia.Net библиотека).
             SearchText.Text = "Введите запрос (Анголязычный поиск)";
         }
-
+        /// <summary>
+        /// Поиск по тексту первого совпадения.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void TextSearchButton_Click(object sender, EventArgs e)
         {
             try
             {
-                TextBox.Select(TextBox.Find(SearchBox.Text), SearchBox.Text.Length);
-                TextBox.SelectionBackColor = Color.Green;
+                TextBox.Select(TextBox.Find(SearchBox.Text), SearchBox.Text.Length); //Выбор нужного фрагмента из всего текста.
+                TextBox.SelectionBackColor = Color.Green; //Изменить цвет заднего фона по умолчанию.
             }
 
             catch
@@ -91,11 +126,15 @@ namespace WindowsFormsPavlovaProject
 
             }
         }
-
+        /// <summary>
+        /// Перезагрузка поиска.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ResetSearchButton_Click(object sender, EventArgs e)
         {
-            TextBox.Select(TextBox.SelectionStart, TextBox.SelectionLength);
-            TextBox.SelectionBackColor = Color.White; 
+            TextBox.Select(TextBox.SelectionStart, TextBox.SelectionLength); //Выбрать закрашеного фрагмента.
+            TextBox.SelectionBackColor = Color.White; // Вернуть цвет заднего фона по умолчанию.
         }
         
 
